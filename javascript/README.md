54a82d64f62996570d5582f90a10d3c044ae4504
Junyoung Lee <junyoung.plum@gmail.com>
2019-11-20 08:33:25 +0900

    기본 환경 세팅

---
+++ javascript/README.md
@@ -0,0 +1,8 @@
+https://firstdoit.com/no-need-for-globals-using-npm-dependencies-in-npm-scripts-3dfb478908
+https://github.com/jasmine/jasmine
+https://mochajs.org
+https://poiemaweb.com/es6-babel-webpack-2
+https://babeljs.io/setup#installation
+https://github.com/browserify/browserify#usage
+https://www.chaijs.com/
+https://edykim.com/ko/post/bdd-with-three-cups-of-coffee-coffeescript-mocha-chai/
---
+++ javascript/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, './src/js')
+                ],
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    mode: 'development'
+}
